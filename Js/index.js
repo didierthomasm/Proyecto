09b4a5fc8274925a6d360dc472cc02866c259641
Didier Thomas <didierthomas.m@gmail.com>
2022-02-10 16:35:44 -0600

    Cambiar sin html

--- Js/index.js
+++ Js/index.js
@@ -1,18 +1,39 @@
-function validarNip (nip){
-    let flag = 1;
+// function validarNip(){
+//     let flag = 1;
+// 	let pin = document.getElementById("pin").value;
+//     console.log(pin);
+//     if (pin === nip) {
+//         flag = 0;
+//     } else {
+//         flag = 1;
+//     }
+// return flag;
+// }
+
+// function oportunidadesNip(nip) {
+//     for (let index = 0; index < 3; index++) {
+//         validarNip(nip);
+//     }
+// }
+
+function validarNip(){
+    //let flag = 0;
 	let pin = document.getElementById("pin").value;
     console.log(pin);
-    if (pin === nip) {
-        flag = 0;
-    } else {
-        flag = 1;
-    }
-return flag;
+    do{
+        if (pin == nip) {
+            flag = 0;
+            alert(`¡Bienvenido!`);
+        } else {
+            flag = flag + 1;
+            alert(`Intente de nuevo, PIN incorrecto`);
+        }
+        console.log("Voy aqui abajo");
+    console.log("Voy hasta abajo");
+    console.log(flag);
+    }while(flag > 3);
+return "Hola munda";
 }
-
-function oportunidadesNip(flag) {
-    for (let index = 0; index < array.length; index++) {
-        const element = array[index];
-        false
-    }
-}
+console.log(validarNip())
+const nip = 1234;
+let flag = 0;
