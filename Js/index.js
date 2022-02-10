a2597a4dc8ba5de5e2c535fd9b1b7815187c8f53
Didier Thomas <didierthomas.m@gmail.com>
2022-02-09 23:30:31 -0600

    primera parte

---
+++ Js/index.js
@@ -0,0 +1,18 @@
+function validarNip (nip){
+    let flag = 1;
+	let pin = document.getElementById("pin").value;
+    console.log(pin);
+    if (pin === nip) {
+        flag = 0;
+    } else {
+        flag = 1;
+    }
+return flag;
+}
+
+function oportunidadesNip(flag) {
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        false
+    }
+}
